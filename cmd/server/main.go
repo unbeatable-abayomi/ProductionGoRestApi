defb95f3977d12b2515094a9c005f3ff184163dc
unbeatable-abayomi <igwubor@outlook.com>
2023-09-12 23:25:54 +0100

    Setting Up Routes

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,13 +1,25 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
+	transportHTTP "github.com/unbeatable-abayomi/ProductionGoRestApi/internal/transport/http"
+	//transportHTTP "/internal/transport/http"
+)
 
 //App struct wjich contains things like pointers
-//to database connections           
+//to database connections
 type App struct{}
 
 func (app *App) Run() error{
     fmt.Println("Setting Up Our App");
+	handler := transportHTTP.NewHandler();
+	handler.SetUpRoutes();
+
+	if err := http.ListenAndServe(":8080", handler.Router); err != nil{
+        fmt.Println("Falied to set up server")
+		return err; 
+	}
 	return nil;                               
 }
 
