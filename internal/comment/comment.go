52cb5f8f5147aae83149cfec024dcef5919527ac
unbeatable-abayomi <igwubor@outlook.com>
2023-09-27 11:52:11 +0100

    Implement Comment Sevrice

--- internal/comment/comment.go
+++ internal/comment/comment.go
@@ -8,10 +8,24 @@ type Service struct{
 	DB *gorm.DB
 }
 
+// Comment defines our comment structure
+type Comment struct{
+	gorm.Model
+	Slug string
+	Body string
+	Author string
+
+}
+
 
 //CommentService --- the interface for our comment service
 type CommentService interface{
-	       
+	GetComment(ID uint) (Comment, error)
+	GetCommentsBySlug(slug string) ([]Comment, error)
+	PostComment(comment Comment) (Comment, error)
+	UpdateComment(ID uint, newComment Comment) (Comment, error)
+	DeleteComment(ID uint) error
+	GetAllComments([]Comment, error)  
 }
 
 //NewService --- returns a new comment service 
